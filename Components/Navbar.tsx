fd566b54a0728aa4928d5243715e2126a68c91b8
Domas <domukas1337@gmail.com>
2025-04-11 22:50:28 +0300

    adapted design for mobile devices

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,17 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black/20 backdrop-blur-lg">
+    <nav className="fixed top-0 left-0 w-full bg-black/20 backdrop-blur-lg text-xl sm:text-3xl">
       <div className="flex justify-between py-4 px-3 text-white">
-        <Link
-          href="/"
-          className="text-3xl hover:text-cyan-200 transition-colors dmkas"
-        >
-          <span className="text-red-600">.</span>DMKAS
+        <Link href="/" className="hover:text-cyan-200 transition-colors dmkas">
+          <span className="text-red-600">.</span>
+          DMKAS
         </Link>
         <Link
           href="/contacts"
-          className="text-3xl uppercase hover:text-cyan-200 transition-colors"
+          className=" uppercase hover:text-cyan-200 transition-colors"
         >
           Contact
         </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <div className="flex flex-col justify-center h-screen first-background text-9xl text-white pl-2">
+      <div className="flex flex-col justify-center h-screen first-background text-6xl sm:text-8xl md:text-9xl text-white pl-2">
         <p>DESIGNING</p>
         <p>RESPONSIVE</p>
         <p>WEBSITES.</p>
@@ -19,8 +19,10 @@ export default function Home() {
           <Link href="/dmkas" className="hover:scale-105 transition-all">
             <Image src="/dmkas.png" alt="bloggy" width={700} height={600} />
           </Link>
-          <p className="text-2xl">Click on image to learn more!</p>
         </div>
+        <p className="text-xl text-center sm:text-2xl">
+          Click on image to learn more!
+        </p>
       </div>
     </main>
   );
