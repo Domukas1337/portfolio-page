740a3d9afa5d52e99d463bbc0e67e264ca8f7639
Domas <domukas1337@gmail.com>
2025-04-11 20:30:12 +0300

    landing page 50% done

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,12 +2,18 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <div className="flex justify-between py-4 px-3">
-        <Link href="/" className="text-2xl">
-          DMKAS
+    <nav className="fixed top-0 left-0 w-full bg-black/20 backdrop-blur-lg">
+      <div className="flex justify-between py-4 px-3 text-white">
+        <Link
+          href="/"
+          className="text-3xl hover:text-cyan-200 transition-colors dmkas"
+        >
+          <span className="text-red-600">.</span>DMKAS
         </Link>
-        <Link href="/contacts" className="text-2xl">
+        <Link
+          href="/contacts"
+          className="text-3xl uppercase hover:text-cyan-200 transition-colors"
+        >
           Contact
         </Link>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import Navbar from "@/Components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <div className="flex flex-col justify-center h-screen first-background text-9xl text-white pl-2">
+        <p>DESIGNING</p>
+        <p>RESPONSIVE</p>
+        <p>WEBSITES.</p>
+      </div>
+      <div className="flex flex-col justify-center h-screen second-background text-white pl-2">
+        <div className="flex justify-center flex-wrap">
+          <Link href="/bloggy" className="hover:scale-105 transition-all">
+            <Image src="/bloggy.png" alt="bloggy" width={700} height={600} />
+          </Link>
+          <Link href="/dmkas" className="hover:scale-105 transition-all">
+            <Image src="/dmkas.png" alt="bloggy" width={700} height={600} />
+          </Link>
+          <p className="text-2xl">Click on image to learn more!</p>
+        </div>
+      </div>
     </main>
   );
 }
