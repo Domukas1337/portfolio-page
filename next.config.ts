d09065c08c3b75af8adafa5b22f84de912aaa062
Domas <domukas1337@gmail.com>
2025-04-21 13:53:16 +0300

    replaced deprecated image pattern to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
   },
 };
 
